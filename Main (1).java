b7208acf6e775f9a9f2bd7156b29b92ed56c9060
Pavan Ahirwar <91248254+Sharad8518@users.noreply.github.com>
2022-02-13 13:49:42 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	 int[] ar ={20,50,30,15,80,30,50,20};
+	 
+	 for(int i=0; i<ar.length-1; i++)
+	       
+	       
+	   
+	    
+	    for(int j=i+1;j<ar.length;j++)
+	   
+	if((ar[i]==ar[j]) && (i!=j)){
+	    
+	    
+	    
+	    System.out.println(" it is Duplicate Number  " +ar[j]);
+	    
+	    
+	}
+	
+	}
+	
+	
+}
